4065037b8816902ea61a4be82925a2585a07d64d
Nicolas Lima <nicolas.pedroso2145@gmail.com>
2021-10-26 14:04:12 -0300

    created model for user

---
+++ src/users/users.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { User } from './user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async create(user: any): Promise<User> {
+    return await this.userRepository.save(user);
+  }
+
+  findById(id: number): Promise<User> {
+    return this.userRepository.findOne(id);
+  }
+
+  async remove(id: number): Promise<void> {
+    await this.userRepository.delete(id);
+  }
+}
